9574d5f1a15e3637dbaa21312ee61ffc5b056dfa
John Templon <jtemplon@gmail.com>
2014-05-12 00:13:37 -0400

    Added a stretch forward to the archetypes

--- player_archetypes.py
+++ player_archetypes.py
@@ -220,6 +220,68 @@ class StarForwardCenter(Player):
                                       "turnover_range": (block_end+1, to_d_end), "steal_range": (steal_d_start, 99)}
         self.game_stats = GameStats()
 
-# star_sg = StarForwardCenter()
-# print star_sg.offense_results.__dict__
-# print star_sg.defense_results.__dict__
+class StarStretchForward(Player):
+    # Not a ton of these in college basketball. Think Josh McRoberts
+    def __init__(self):
+        self.name = names.get_full_name(gender='male')
+        self.primary_position = "f"
+        self.shot_rating = random.randint(6,14)
+        three_point_shot = random.randint(2,5)
+        self.three_point_rating = self.shot_rating + three_point_shot
+        self.shots_per_game = random.randint(7,14)
+        self.defensive_rebound_rating = random.randint(9,20)
+        self.offensive_rebound_rating = random.randint(3,13)
+        self.assist_rating = random.randint(5,20)
+        self.offense_results.three_point_shot = (1, 38+random.randint(0,17))
+        self.offense_results.free_throw_shot = (1, random.randint(68,78))
+        self.defense_results.three_point_shot = (1, random.randint(28,35))
+        fgf_o_tweak = random.randint(0,8)
+        fg_o_range = random.randint(6,14)
+        fg_o_end = 11 + fgf_o_tweak + fg_o_range
+        assist_range = random.randint(5,10)
+        assist_end = fg_o_end + 1 + assist_range
+        to_range = random.randint(0,13)
+        o_foul_range = random.randint(1,10)
+        steal_range = random.randint(2,11)
+        steal_start = 99 - steal_range
+        to_start = steal_start - 1 - to_range
+        o_foul_start = to_start - 1 - o_foul_range
+        miss_range = random.randint(14,26)
+        miss_start = o_foul_start - 1 - miss_range
+        if (miss_start-1) < (assist_end+1):
+            miss_start = assist_end+2
+        self.offense_results.plays = {"fg_foul_range": (1, 10+fgf_o_tweak), "fg_range": (11+fgf_o_tweak, fg_o_end), 
+                                      "assist_range": (fg_o_end+1, assist_end), "foul_range": (assist_end+1, miss_start-1), 
+                                      "miss_range": (miss_start, o_foul_start-1), "block_range": None, 
+                                      "offensive_foul_range": (o_foul_start, to_start-1), "turnover_range": (to_start, steal_start-1), 
+                                      "steal_range": (steal_start, 99)}
+        fg_d_tweak = random.randint(0,8)
+        foul_d_tweak = random.randint(5,21)
+        miss_d_range = random.randint(4,36)
+        miss_d_start = fg_d_tweak + foul_d_tweak + 28
+        miss_d_end = miss_d_start + miss_d_range
+        block_range = random.randint(30,36) - miss_d_range
+        if block_range < 0:
+            block_range = 0
+        block_end = miss_d_end + 1 + block_range
+        to_d_range = random.randint(8,21)
+        to_d_end = block_end + 1 + to_d_range
+        #this deals with guys that block a ton of shots
+        if block_end > 98:
+            block_end = 98
+            to_d_end = 99
+            steal_d_start = 99
+        elif to_d_end > 98:
+            to_d_end = 99
+            steal_d_start = 99
+        else:
+            steal_d_start = to_d_end+1
+        self.defense_results.plays = {"fg_foul_range": None, "fg_range": (1, 23+fg_d_tweak), "assist_range": None,
+                                      "foul_range": (28+fg_d_tweak, 28+fg_d_tweak+foul_d_tweak), "miss_range": (miss_d_start, miss_d_end), 
+                                      "block_range": (miss_d_end+1, block_end), "offensive_foul_range": None, 
+                                      "turnover_range": (block_end+1, to_d_end), "steal_range": (steal_d_start, 99)}
+        self.game_stats = GameStats()
+
+star_sg = StarStretchForward()
+print star_sg.offense_results.__dict__
+print star_sg.defense_results.__dict__
